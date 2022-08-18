9829b1ee344424b2c1e3f5b252974ccc1944b06b
Gabriela L. T. V. Mesquita <gabriela.mesquita@luizalabs.com>
2022-08-18 19:13:47 -0300

    feat: routes navigation and layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom'
+import { HeaderContainer } from './styles'
+import Logo from '../../assets/logo.png'
+import { Timer, Scroll } from 'phosphor-react'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={Logo} alt="" width="80" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={30} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={30} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
